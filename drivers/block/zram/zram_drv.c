a18b242d9825a7ede040acd57c56be201a810c90
Nathan Chancellor <natechancellor@gmail.com>
2018-06-27 09:59:16 -0700

    zram: Use lz4 compression by default if present
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>
    Signed-off-by: MadeOfGreat <ravenklawasd@gmail.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -43,7 +43,9 @@ static int zram_major;
 #ifdef CONFIG_CRYPTO_LZ4
 static const char *default_compressor = "lz4";
 #else
-static const char *default_compressor = CONFIG_ZRAM_DEFAULT_COMP_ALGORITHM;
+
+static const char *default_compressor = "lzo";
+
 #endif
 
 /* Module params (documentation at end) */
