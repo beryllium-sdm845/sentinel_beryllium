5005408fff0baf25164d109f3b3e1243d0f6d4a1
YumeMichi <do4suki@gmail.com>
2019-05-06 21:16:00 +0800

    drm: Make exposure adjustment switchable
    Change-Id: I7e71f6ca82a77cc1660b8910202f1325729ce49f
    Signed-off-by: YumeMichi <do4suki@gmail.com>
    Signed-off-by: Michael <loukerismichalis@gmail.com>
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>
    Signed-off-by: MadeOfGreat <ravenklawasd@gmail.com>
    Signed-off-by: CloudedQuartz <ravenklawasd@gmail.com>
    Signed-off-by: sajidshahriar72543 <sazidshahriar39@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/exposure_adjustment.h
+++ drivers/gpu/drm/msm/dsi-staging/exposure_adjustment.h
@@ -47,4 +47,7 @@ void ea_panel_mode_ctrl(struct dsi_panel *panel, bool enable);
 u32 ea_panel_calc_backlight(u32 bl_lvl);
 
 
+unsigned int ea_panel_on(void);
+
+
 #endif /* EXPOSURE_ADJUSTMENT_H */
