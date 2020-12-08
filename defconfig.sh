3b895eeef2a71e035996413bc7961e481cf0174a
sajidshahriar72543 <sazidshahriar39@gmail.com>
2020-12-08 15:18:47 +0600

    arch/arm64: Sync beryllium defconfig

--- defconfig.sh
+++ defconfig.sh
@@ -5,8 +5,8 @@ actual_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 KERNEL_SOURCE_DIR=/home/sajid/KERNEL/android_kernel_xiaomi_sdm845
 
 cd ${KERNEL_SOURCE_DIR}
-ARCH=arm64 make defconfig KBUILD_DEFCONFIG=noto_defconfig
-mv .config ${KERNEL_SOURCE_DIR}/arch/arm64/configs/noto_defconfig
+ARCH=arm64 make defconfig KBUILD_DEFCONFIG=beryllium_defconfig
+mv .config ${KERNEL_SOURCE_DIR}/arch/arm64/configs/beryllium_defconfig
 
 ARCH=arm64 make defconfig KBUILD_DEFCONFIG=beryllium_stock_defconfig
 mv .config ${KERNEL_SOURCE_DIR}/arch/arm64/configs/beryllium_stock_defconfig
