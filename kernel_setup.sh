97d832cc0f6db55a20c544b08f2e86c0dde65cc7
sajidshahriar72543 <sazidshahriar39@gmail.com>
2021-02-02 13:24:37 +0600

    Revert "drm: msm: kcal: Make the userspace module param vars static and use short instead of integers"
    This reverts commit 379b2d3f13e0515e8ea3d4a171e4730e7d5f4f64.
    
    Signed-off-by: sajidshahriar72543 <sazidshahriar39@gmail.com>

--- kernel_setup.sh
+++ kernel_setup.sh
@@ -10,7 +10,7 @@
 CURRENT_DIR="$(pwd)"
 KERNELNAME="Notorious"
 KERNEL_DIR="$CURRENT_DIR"
-AK_REPO="https://github.com/xiaomi-sdm845-devel/AnyKernel3"
+AK_REPO="https://github.com/sajidshahriar72543/AnyKernel3"
 AK_DIR="$HOME/AnyKernel3"
 TC_DIR="$HOME/proton-clang"
 # End Config
